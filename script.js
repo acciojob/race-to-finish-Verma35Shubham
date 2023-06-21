7189a72ec9e619a8c7b6e6b65d7ff3513059df19
Shubham Verma <verma.shubham2537@gmail.com>
2023-06-21 16:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-const textInput = document.getElementById('text');
-const delayInput = document.getElementById('delay');
-const outputDiv = document.getElementById('output');
-const btn = document.getElementById('btn');
+window.promises = [];
 
-btn.addEventListener('click', async () => {
-  const text = textInput.value;
-  const delay = delayInput.value;
+// Do not change the code above this
+// add your promises to the array `promises`
+const prom1 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		
+		resolve("One")},(Math.floor(Math.random() * 5000)))
+})
 
-  await new Promise(resolve => setTimeout(resolve, delay));
+const prom2 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("two")
+	},(Math.floor(Math.random() * 5000)))
+})
 
-  outputDiv.innerText = text;
-});
+const prom3 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("three")
+	},(Math.floor(Math.random() * 5000)));
+})
+
+const prom4 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("four")
+	},(Math.floor(Math.random() * 5000)))
+})
+
+const prom5 = new Promise((resolve, reject)=>{
+	setTimeout(()=>{
+		resolve("five")
+	},(Math.floor(Math.random() * 5000)))
+})
+
+window.promises = [prom1, prom2, prom3, prom4, prom5 ];
+Promise.any	(promises)
+.then((value)=>{
+	document.getElementById("output").innerText = value ;
+})
+.catch((err)=>{
+	console.log("error");
+})
